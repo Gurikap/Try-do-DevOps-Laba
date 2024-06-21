9ae67c2671decb4d6fceec92339585d9d51bdfbb
Gurikap <154047638+Gurikap@users.noreply.github.com>
2024-06-21 10:50:19 +0300

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -2,17 +2,17 @@ import unittest
 from main import summ, diff
 
 class TestMain(unittest.TestCase):
-  def Test_s1(self):
+  def test_s1(self):
     self.assertEqual(summ(1, 3), 4)
-  def Test_s2(self):
+  def test_s2(self):
     self.assertEqual(summ(10, 100), 110)
-  def Test_s3(self):
+  def test_s3(self):
     self.assertEqual(summ(13, 7), 20)
-  def Test_d1(self):
+  def test_d1(self):
     self.assertEqual(diff(17, 1), 16)
-  def Test_d2(self):
+  def test_d2(self):
     self.assertEqual(diff(13, 4), 9)
-  def Test_d3(self):
+  def test_d3(self):
     self.assertEqual(diff(15, 5), 10)
 
 
